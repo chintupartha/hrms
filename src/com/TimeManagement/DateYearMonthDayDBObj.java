659a66a9a4804c6e2f517a3102a7a3e6123ab965
chintupartha <parthapratim.baral@yahoo.in>
2018-02-27 00:01:17 +0530

    Revert "Source code commit"
    This reverts commit eeb7cffe4e8ad5815555f4bd96065018d1aa1352.

--- src/com/TimeManagement/DateYearMonthDayDBObj.java
+++
@@ -1,8 +0,0 @@
-package com.TimeManagement;
-
-public class DateYearMonthDayDBObj{
-	public String today_date ;    
-	public String month  ;    
-	public String day ;       
-	public long year ;
-}
--- src/com/main/Exceptions/HrmsBaseException.java
+++
@@ -1,8 +0,0 @@
-package com.main.Exceptions;
-
-public class HrmsBaseException extends Exception{
-	
-	public HrmsBaseException(String msg){
-		super(msg);
-	}
-}
